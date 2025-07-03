9134db4db6f2b87cf9a0084a107643ec60e322ee
rubentrottein <38758157+rubentrottein@users.noreply.github.com>
2025-07-03 03:11:38 +0200

    Version 2.0 graphique

---
+++ js/script.js
@@ -0,0 +1,30 @@
+for (let img of document.querySelectorAll("img")){
+    img.addEventListener("click", ()=>{
+        window.open(img.src, '_blank');
+    })
+}
+
+//TODO au clic sur aaron_scans afficher le .hide dans une modale
+document.querySelector(".aaron_scans").addEventListener("click", (e)=>{
+    e.preventDefault();
+    const hide = document.querySelector(".hide");
+    hide.classList.toggle("show");
+    if (hide.classList.contains("show")){
+        hide.style.display = "block";
+    } else {
+        hide.style.display = "none";
+    }
+});
+
+
+const soundCloud = document.querySelector(".SoundCloud");
+const soundCloudToggle = document.querySelector(".SoundCloudToggle");
+soundCloudToggle.addEventListener("click", (e)=>{
+    e.preventDefault();
+    soundCloud.classList.toggle("hide");
+    if (soundCloud.classList.contains("hide")){
+        soundCloud.style.display = "none";
+    } else {
+        soundCloud.style.display = "block";
+    }
+});
